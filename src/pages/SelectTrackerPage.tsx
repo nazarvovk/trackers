28580030acaba4b8d0d513bc21fd1e2b876ddb4a
Nazar Vovk <nazarvovk.work@gmail.com>
2020-04-26 22:28:41 +0300

    Analytics and Author

--- src/pages/SelectTrackerPage.tsx
+++ src/pages/SelectTrackerPage.tsx
@@ -5,6 +5,13 @@ const SelectTrackerPage: React.FC = () => {
   return (
     <main className="SelectTrackerPage">
       <p>Select a tracker or create a new one</p>
+
+      <p className="author">
+        Made by
+        <a target="_blank" rel="noopener noreferrer" href="http://nvovk.com/">
+          Nazar Vovk
+        </a>
+      </p>
     </main>
   );
 };
